103d7ec4fe59e3c90b8344aaaf03de668e96b6d6
Prathamesh Patankar <prathamesh.patankar@nitorinfotech.com>
2025-12-23 13:49:10 +0530

    added the feature for viewing uploaded files

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ from backend.app.api import export_data as export_api
 from backend.app.api import dashboard as dashboard_api
 from backend.app.db.base import Base  
 from backend.app.db.session import engine
-
+from backend.app.api import pmo_docs as file_list_api
+from pathlib import Path
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title="AI Insight Platform API",
@@ -26,6 +28,22 @@ app.include_router(dashboard_api.router, prefix="/v1")
 # app.include_router(sow_extractor_api.router,  prefix="/v1")
 app.include_router(export_api.router, prefix="/v1")
 # app.include_router(sow_api.router,  prefix="/v1")
+
+# Define Paths
+BACKEND_DIR = Path(__file__).resolve().parent.parent
+PMO_DIR = BACKEND_DIR / "uploaded_docs" / "app_docs" / "pmo"
+REVENUE_DIR = BACKEND_DIR / "uploaded_docs" / "app_docs" / "revenue"
+
+# Create folders
+PMO_DIR.mkdir(parents=True, exist_ok=True)
+REVENUE_DIR.mkdir(parents=True, exist_ok=True)
+
+# Mount them (This enables the download functionality)
+app.mount("/static_pmo", StaticFiles(directory=PMO_DIR), name="static_pmo")
+app.mount("/static_revenue", StaticFiles(directory=REVENUE_DIR), name="static_revenue")
+
+# Include the Router
+app.include_router(file_list_api.router, tags=["Project Documents"])
 
 # Call the function to initialize tables. 
 # init_db()
